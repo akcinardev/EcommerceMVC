bc6d675d2a5921ff162d0f849294f819b15c65b3
akcinar.omer <akcinardev@gmail.com>
2024-08-12 15:05:21 +0200

    CategoryController added

---
+++ EcommerceWeb/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
--- EcommerceWeb/Program.cs
+++ EcommerceWeb/Program.cs
@@ -1,3 +1,6 @@
+using EcommerceWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace EcommerceWeb
 {
     public class Program
@@ -8,6 +11,11 @@ namespace EcommerceWeb
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
 
             var app = builder.Build();
 
